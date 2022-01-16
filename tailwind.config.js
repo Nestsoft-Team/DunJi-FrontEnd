521b86ac3cdfc8ca06ae1bc4ec168fdecafd77e3
junbox98221@gmail.com <junbox98221@gmail.com>
2022-01-16 22:26:40 +0900

    기본 프로젝트 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                yellow: "#fbd40a",
+                blur: "rgba(0,0,0,0.4)",
+                sky_blue: "#8ec2f6",
+            },
+            fontFamily: {
+                sans: ["SegoeUI"],
+                HANDotum: ["HANDotum"],
+            },
+            width: {
+                main_width: "25rem",
+            },
+        },
+    },
+    plugins: [],
+};
