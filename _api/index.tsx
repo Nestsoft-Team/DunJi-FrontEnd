3cafdceb7c7a5f89fce117113166cc92a4b142fb
junbox98221@gmail.com <junbox98221@gmail.com>
2022-03-08 20:49:02 +0900

    <#3> 카카오 로그인 인가코드 전달 api 구현

--- _api/index.tsx
+++ _api/index.tsx
@@ -1,3 +1,9 @@
 import axios from "axios";
+const api = axios.create({
+    baseURL: "http://218.235.241.107:8090",
+});
 
-const api = axios.create({});
+export const logInApi = {
+    postPermissionCode: (code: any) =>
+        api.get("/login/oauth_kakao", { params: { code: code } }),
+};
--- pages/oauth/kakao/callback/index.tsx
+++ pages/oauth/kakao/callback/index.tsx
@@ -1,10 +1,28 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { logInApi } from "_api";
+export default function Auth() {
+    const getCode = () => {
+        const id = window.location.href.split("callback?code=")[1];
+        return id;
+    };
 
-const Auth = () => {
-    const {
-        query: { code },
-    } = useRouter();
+    useEffect(() => {
+        async function postCode() {
+            const code = getCode();
+            console.log(code);
+            try {
+                console.log("start");
+                let result = await logInApi.postPermissionCode(code);
+                console.log(result);
+                console.log("end");
+            } catch {
+                alert("로그인을 실패하였습니다. 다시 시도해 주세요.");
+            } finally {
+            }
+        }
+        postCode();
+    }, []);
 
-    return <div>{code}</div>;
-};
-export default Auth;
+    return <div></div>;
+}
