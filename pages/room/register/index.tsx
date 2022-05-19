0a92eeef7f27fd5152c83906e199a20c53c5eba1
junbox98221@gmail.com <junbox98221@gmail.com>
2022-05-19 16:43:30 +0900

    <#17> post api 예시
    rebase위한 커밋

--- pages/room/register/index.tsx
+++ pages/room/register/index.tsx
@@ -6,10 +6,22 @@ import BottomNextBtn from "components/room-register/BottomNextBtn";
 import DetailInfo from "components/room-register/extraComponents";
 import BasicInfo from "components/room-register/basicComponents";
 import PhotoRegister from "components/room-register/PhotoComponents";
+import { useEffect } from "react";
+import { roomApi } from "_api";
 
 export default function Register() {
     const roomRegister = useSelector((state: RootState) => state.roomRegister);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                let response = await roomApi.postRoom();
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fetchData();
+    });
     return (
         <div className="pb-[12rem] min-h-screen">
             <Header />
