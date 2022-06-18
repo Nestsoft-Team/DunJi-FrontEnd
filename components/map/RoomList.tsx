bfce0ea073eac20f777b4c6c7ab3c743d879ce2e
junbox98221@gmail.com <junbox98221@gmail.com>
2022-06-18 21:58:44 +0900

    <#11> 클러스터러, 오버레이 샘플 생성, 오버레이 클릭 시 간략한 정보 컴포넌트 출력

--- components/map/RoomList.tsx
+++ components/map/RoomList.tsx
@@ -9,11 +9,12 @@ type propsType = {
 };
 export default function RoomList({ openPopup }: propsType) {
     const [upDown, setUpDown] = useState(0); // 컴포넌트 올라온 단계(0: 지도 하단 가림, 1: 지도 절반 가림 2: 지도 가림)
-    const [touchY, setTouchY] = useState(10000);
+    const [touchY, setTouchY] = useState(10000); // n개의 방 컴포넌트 터치 이벤트에서 얻은 y 값
     const [margin, setMargin] = useState({
         marginTop: "calc(100vh - 15.5rem)",
     });
     const [innerHeight, setInnerHeight] = useState(0);
+    const [windowHeight, setWindowHeight] = useState(0); // 화면 전체 y값
 
     useEffect(() => {
         const handleResize = () => {
@@ -28,13 +29,15 @@ export default function RoomList({ openPopup }: propsType) {
 
     return (
         <div className="w-full  z-10" style={margin}>
-            {upDown === 0 && openPopup && touchY === 0 && <Popup />}
+            {upDown === 0 && openPopup && touchY === windowHeight && <Popup />}
             <RoomListHeader
                 setUpDown={setUpDown}
                 touchY={touchY}
                 setTouchY={setTouchY}
                 setMargin={setMargin}
                 innerHeight={innerHeight}
+                windowHeight={windowHeight}
+                setWindowHeight={setWindowHeight}
             />
             <RoomListSlider />
             <RoomListYSlider />
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -6,7 +6,7 @@ import Header from "components/map/Header";
 export default function Map() {
     const [latitude, setLatitude] = useState(37.297526827747966); //한양대 에리카 위도,경도
     const [longitude, setLongitude] = useState(126.835628984629);
-    const [openPopup, setOpenPopup] = useState(true);
+    const [openPopup, setOpenPopup] = useState(false);
 
     useEffect(() => {
         const getLocation = () => {
@@ -37,8 +37,9 @@ export default function Map() {
     useEffect(() => {
         const mapScript = document.createElement("script");
         mapScript.async = true;
-        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
+        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&libraries=services,clusterer&autoload=false`;
         document.head.appendChild(mapScript);
+
         const onLoadKakaoMap = () => {
             window.kakao.maps.load(() => {
                 const container = document.getElementById("map");
@@ -47,31 +48,76 @@ export default function Map() {
                     level: 6,
                 };
                 const map = new window.kakao.maps.Map(container, options);
-                const markerPosition = new window.kakao.maps.LatLng(
+
+                function overlayClickHandler() {
+                    // router.replace("/room/1");
+                    setOpenPopup(!openPopup);
+                }
+
+                const content = document.createElement("div");
+                content.innerText = "   14만 / 주";
+                content.onclick = overlayClickHandler;
+                content.style.fontSize = "1.3rem";
+                content.style.backgroundColor = "#3F3C3A";
+                content.style.color = "#F9F9F9";
+                content.style.padding = "0.5rem 1.5rem";
+                content.style.borderRadius = "1.2rem";
+                content.style.opacity = "0.6";
+
+                const position = new window.kakao.maps.LatLng(
                     latitude,
                     longitude
                 );
-                const marker = new window.kakao.maps.Marker({
-                    position: markerPosition,
+
+                const customOverlay = new window.kakao.maps.CustomOverlay({
+                    position: position,
+                    content: content,
                 });
-                marker.setMap(map);
-                window.kakao.maps.event.addListener(map, "dragend", () =>
-                    console.log(map.getBounds())
-                ); // 드래그 이동 시 좌표 출력
-                window.kakao.maps.event.addListener(map, "zoom_changed", () =>
-                    console.log(map.getBounds())
-                ); // 줌 변경 시 좌표 출력
+
+                var clusterer = new kakao.maps.MarkerClusterer({
+                    map: map,
+                    averageCenter: true,
+                    minLevel: 7,
+                    minClusterSize: 1,
+                    styles: [
+                        {
+                            color: "#F9F9F9",
+                            background: "#3F3C3A",
+                            width: "3rem",
+                            height: "3rem",
+                            "border-radius": "100%",
+                            display: "flex",
+                            "align-items": "center",
+                            "justify-content": "center",
+                        },
+                    ],
+                });
+
+                customOverlay.setMap(map);
+                clusterer.setMap(map);
+                clusterer.addMarkers([customOverlay]);
+                window.kakao.maps.event.addListener(map, "dragend", () => {
+                    console.log(map.getBounds());
+                }); // 드래그 이동 시 좌표 출력
+                window.kakao.maps.event.addListener(map, "zoom_changed", () => {
+                    console.log(map.getBounds());
+                }); // 줌 변경 시 좌표 출력
             });
         };
         mapScript.addEventListener("load", onLoadKakaoMap);
         return () => mapScript.removeEventListener("load", onLoadKakaoMap);
-    }, [latitude, longitude]);
+    }, [latitude, longitude, openPopup]);
 
     return (
-        <div className="flex flex-col w-screen  h-screen relative overflow-hidden">
-            <Header />
-            <RoomList openPopup={openPopup} />
-            <div id="map" className="absolute top-0 w-screen h-screen" />
-        </div>
+        <>
+            <div className="flex flex-col w-screen  h-screen relative overflow-hidden">
+                <Header />
+                <RoomList openPopup={openPopup} />
+                <div
+                    className="absolute top-0 w-screen h-screen"
+                    id="map"
+                ></div>
+            </div>
+        </>
     );
 }
